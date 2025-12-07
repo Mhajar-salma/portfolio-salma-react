e09ceccbcd724ec44650103036cd59021f78e526
salmaiii <salma.mhajar9@gmail.com>
2025-12-07 12:22:50 +0100

    fix GitHub Pages config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio-salma-react/", // <<< très important pour GitHub Pages
+  base: "/portfolio-salma-react/",
 });
